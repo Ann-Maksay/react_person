5f014e967ad4c416fa12b7569834bc6627450ae4
Anna Maksai <annamaksai99@gmail.com>
2024-09-09 09:57:47 +0300

    fix: partner, use person element n app

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,12 +8,10 @@ export const Person = ({ person }) => (
       <p className="Person__age">I am {person.age}</p>
     )}
 
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
+    <p className="Person__partner">
+      {person.isMarried
+        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+        : 'I am not married'}
+    </p>
   </section>
 );
